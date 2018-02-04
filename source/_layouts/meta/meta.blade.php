f5db66e19baa98ff7cf75c0b0ce5e78670c1c428
Thomas Farla <thomas-farla@hotmail.com>
2018-02-04 23:28:34 +0100

    Change image seo

--- source/_layouts/meta/meta.blade.php
+++ source/_layouts/meta/meta.blade.php
@@ -9,11 +9,11 @@
 <meta property="og:description" content="{{ $page->description() }}" />
 <meta property="og:url" content="{{ $page->getUrl() }}" />
 <meta property="og:site_name" content="Pedicuresalon Farla" />
-<meta property="og:image" content="{{ $page->url('share-farla.jpg') }}" />
+<meta property="og:image" content="{{ $page->url('images/share-farla.jpg') }}" />
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
 
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:description" content="{{ $page->description() }}" />
 <meta name="twitter:title" content="{{ $page->title() }}" />
-<meta name="twitter:image" content="{{ $page->url('share-farla.jpg') }}" />
+<meta name="twitter:image" content="{{ $page->url('images/share-farla.jpg') }}" />
