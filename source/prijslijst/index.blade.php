b0ed2302f9550bf9e139bffaf109a2e13ca56e5c
Thomas Farla <thomas-farla@hotmail.com>
2018-02-04 18:27:01 +0100

    Add pricelist

---
+++ source/prijslijst/index.blade.php
@@ -0,0 +1,83 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="container container-card top30 bottom30">
+    <div class="row">
+        <table class="table table-responsive">
+            <tbody>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-basis-all-in/') }}" target="_blank" rel="noopener">Pedicure Basis All-in</a></td>
+                    <td>&euro; 21,-</td>
+                    <td><a href="{{ $page->url('/behandeling/pedicure-basis-all-in/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}" target="_blank" rel="noopener">Pedicure deluxe</a></td>
+                    <td>&euro; 27,-</td>
+                    <td><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}" target="_blank" rel="noopener">Pedicure la couleur</a></td>
+                    <td>&euro; 27,-</td>
+                    <td><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <th colspan="6"><b>Gel nail repair</b></th>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/gel-nail-repair/') }}" target="_blank" rel="noopener">grote teennagel</a></td>
+                    <td>&euro; 15,-</td>
+                    <td><a href="{{ $page->url('/behandeling/gel-nail-repair/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/gel-nail-repair/') }}" target="_blank" rel="noopener">beide grote teennagels</a></td>
+                    <td>&euro; 25,-</td>
+                    <td><a href="{{ $page->url('/behandeling/gel-nail-repair/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <th colspan="6"><b>Body sugaring</b></th>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">wenkbrauwen</a></td>
+                    <td>&euro; 10,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">bovenlip</a></td>
+                    <td>&euro; 10,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">kin</a></td>
+                    <td>&euro; 10,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">volledig gezicht</a></td>
+                    <td>&euro; 20,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">onder benen</a></td>
+                    <td>&euro; 18,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">volledige benen</a></td>
+                    <td>&euro; 35,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">neus</a></td>
+                    <td>&euro; 5,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">oren</a></td>
+                    <td>&euro; 5,-</td>
+                    <td><a href="{{ $page->url('/behandeling/body-sugaring/') }}" target="_blank" rel="noopener">Lees meer</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
