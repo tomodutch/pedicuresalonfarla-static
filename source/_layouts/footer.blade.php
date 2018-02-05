e23cbc8d0f9600fa11305689ef664402bb826381
Thomas Farla <thomas-farla@hotmail.com>
2018-02-05 07:40:19 +0100

    Center footer

--- source/_layouts/footer.blade.php
+++ source/_layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="footer" style="background-color: #2C3E50; padding-top:15px">
     <div class="container">
         <div class="row">
-            <div class="col-xs-4">
-                <ul class="list-unstyled">
+            <div class="col-xs-12">
+                <ul class="list-unstyled text-center">
                     <li><i class="fas fa-phone-volume"></i> 06 24526696</li>
                     <li><i class="fas fa-road"></i> 4695BJ Rädda Barnenstraat 1</li> 
                     <li><i class="fas fa-building"></i> Sint-Maartensdijk</li>
