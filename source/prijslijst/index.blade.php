bffdf5f9b54ca700ac5e0da1295e2ce51ffced18
Thomas Farla <4613944+TFarla@users.noreply.github.com>
2021-03-11 18:28:40 +0100

    Update prices

--- source/prijslijst/index.blade.php
+++ source/prijslijst/index.blade.php
@@ -7,18 +7,18 @@
             <tbody>
                 <tr>
                     <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-basis-all-in/') }}">Pedicure Basis All-in</a></td>
-                    <td>&euro; 22,-</td>
+                    <td>&euro; 23,-</td>
                     <td><a href="{{ $page->url('/behandeling/pedicure-basis-all-in/') }}">Lees meer</a></td>
                 </tr>
                 <tr>
-                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}">Pedicure deluxe</a></td>
-                    <td>&euro; 29,-</td>
-                    <td><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}">Lees meer</a></td>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}">Pedicure la couleur</a></td>
+                    <td>&euro; 30,-</td>
+                    <td><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}">Lees meer</a></td>
                 </tr>
                 <tr>
-                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}">Pedicure la couleur</a></td>
-                    <td>&euro; 29,-</td>
-                    <td><a href="{{ $page->url('/behandeling/pedicure-la-couleur/') }}">Lees meer</a></td>
+                    <td colspan="3"><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}">Pedicure deluxe</a></td>
+                    <td>&euro; 32,-</td>
+                    <td><a href="{{ $page->url('/behandeling/pedicure-deluxe/') }}">Lees meer</a></td>
                 </tr>
             </tbody>
         </table>
